a1a2ac3bd3ce279dae97a1f6eb3a4b04ea216762
shanharvs <harvey.shanizza@ue.edu.ph>
2025-06-27 23:08:57 +0800

    Update about.php

--- about.php
+++ about.php
@@ -90,18 +90,16 @@ session_start();
   <div class="container">
     <div class="row">
       <div class="col-md-8">
-        <p>LAGYAN MO HERE NG SHORT DESCRIP/BACKGROUND NG COMPANY/THEN PALITAN MO LANG LOGO consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-          dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat.</p>
-        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-          Excepteur sint occaecat cupidatat non proident.</p>
-        <p>Deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-          architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia.</p>
+        <p>Welcome to your new happy place! Founded in January 2025, we are your go-to place for all things sweet. 
+          From classic candies and creamy chocolates to fun and flavorful treats, we bring joy to every bite.</p>
+        <p>We believe that every sweet treat should make you smile. 
+          That’s why we focus on offering only the highest quality treats that you’ll love sharing, gifting, or enjoying for yourself.</p>
+        <p>Whether you’re looking for a nostalgic favorite or something new to try, our wide selection has something for every sweet tooth. 
+          Life is simply better with a sweet tooth, and we’re here to prove it.</p>
       </div>
       <div class="col-md-4 text-center drag-lg-top">
         <div class="shadow-down mb-4">
-          <img src="images/about/author.jpg" alt="author" class="img-fluid w-100 rounded-lg border-thick border-white">
+          <img src="pics/logo3.png" alt="author" class="img-fluid w-100 rounded-lg border-thick border-white">
         </div>
         <h4>Sweet Treats</h4>
       </div>
